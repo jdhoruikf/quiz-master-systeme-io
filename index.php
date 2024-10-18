4887aaa05e26d79a70d1ba0f7739316173f9f4ee
jdhoruikf <jdhoruikf@gmail.com>
2024-10-18 22:41:50 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en-US" data-reactroot="">
+
+<head>
+    <title></title>
+    <meta data-react-helmet="true" name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <meta data-react-helmet="true" name="robots" content="noindex">
+    <meta data-react-helmet="true" name="title" content="MasterQuiz">
+    <meta data-react-helmet="true" name="description" content="">
+    <meta data-react-helmet="true" name="author" content="">
+    <meta data-react-helmet="true" name="keywords" content="">
+    <meta data-react-helmet="true" property="og:image" content="%25BLOG_POST_IMAGE_URL%25.html">
+    <meta data-react-helmet="true" property="og:image:alt" content="MasterQuiz">
+    <meta data-react-helmet="true" property="og:title" content="MasterQuiz">
+    <meta data-react-helmet="true" property="og:description" content="">
+    <meta data-react-helmet="true" property="og:url" content="https://quiz-master.systeme.io/master-quiz">
+    <meta data-react-helmet="true" name="format-detection" content="telephone=no">
+    <style data-styled="dICPES lbczSC gDcHhm cbAvIN Rgtyf brjtYI imLhLp uqayJ bmWsin kQOYHh hahKoj kDplOt gemMjA" data-styled-version="4.4.0">
+        /* sc-component-id: sc-global-1998913337 */
+        html{box-sizing:border-box;-webkit-scroll-behavior:smooth;-moz-scroll-behavior:smooth;-ms-scroll-behavior:smooth;scroll-behavior:smooth;} body{margin:0;} a{-webkit-text-decoration:none;text-decoration:none;} p{margin:0;} *,*:before,*:after{box-sizing:inherit;} *{-webkit-font-smoothing:antialiased;} h1,h2,h3,h4,h5,h6{margin:0;font-weight:normal;} section h1{margin:0;} button,input{margin:0;font-family:unset;} ul{margin:0;} .fixed{position:fixed;top:0px;left:0px;width:320px;height:50px;background:red;-webkit-backface-visibility:hidden;}
+        /* sc-component-id: sc-jwKygS */
+        .gDcHhm{position:absolute;z-index:0;height:100%;width:100%;top:0;left:0;background-size:initial;background-color:rgba(0,0,0,1);background-repeat:repeat;background-position:50% 50%;background-attachment:initial;width:100%;z-index:-1;-webkit-filter:blur(0px);filter:blur(0px);background-image:none;} @media only screen and (max-width:800px){.gDcHhm{background-color:rgba(0,0,0,1);background-image:none;background-size:initial;background-repeat:repeat;background-position:50% 50%;background-attachment:initial;width:100%;}} @media only screen and (min-device-width:768px) and (max-device-width:1024px){}
+        /* sc-component-id: sc-btzYZH */
+        .lbczSC{position:relative;width:100%;background-size:initial;background-repeat:repeat;background-position:50% 50%;background-attachment:initial;width:100%;} @media only screen and (max-width:800px){.lbczSC{background-size:initial;background-repeat:repeat;background-position:50% 50%;background-attachment:initial;width:100%;}}
+        /* sc-component-id: sc-laTMn */
+        .kQOYHh{display:-webkit-box;display:-webkit-flex;display:-ms-flexbox;display:flex;-webkit-flex-direction:column;-ms-flex-direction:column;flex-direction:column;grid-column-start:1;grid-column-end:13;-webkit-align-self:flex-start;-ms-flex-item-align:start;align-self:flex-start;} @media only screen and (max-width:800px){.kQOYHh{-webkit-align-self:unset;-ms-flex-item-align:unset;align-self:unset;width:100%;grid-column-start:1;grid-column-end:12;padding-left:0;padding-right:0;}}
+        /* sc-component-id: sc-cpmKsF */
+        .gemMjA{display:block;max-width:100%;object-fit:contain;width:879px;} @media only screen and (max-width:800px){}
+        /* sc-component-id: sc-kQsIoO */
+        .hahKoj{-webkit-align-self:center;-ms-flex-item-align:center;align-self:center;} @media only screen and (max-width:800px){}
+        /* sc-component-id: sc-gPzReC */
+        .kDplOt{position:relative;display:block;height:0;overflow:visible;padding-bottom:39.06%;}
+        /* sc-component-id: sc-gbOuXE */
+        .bmWsin{display:grid;-webkit-box-pack:justify;-webkit-justify-content:space-between;-ms-flex-pack:justify;justify-content:space-between;grid-template-columns:repeat(12,1fr);-webkit-column-gap:20px;column-gap:20px;padding-top:10px;padding-right:10px;padding-bottom:10px;padding-left:10px;background-image:none;-webkit-column-gap:unset !important;column-gap:unset !important;} @media only screen and (max-width:800px){.bmWsin{padding-top:5px;padding-right:5px;padding-bottom:5px;padding-left:5px;}} @media only screen and (max-width:800px){.bmWsin{display:-webkit-box;display:-webkit-flex;display:-ms-flexbox;display:flex;-webkit-flex-direction:column;-ms-flex-direction:column;flex-direction:column;grid-template-columns:auto;background-image:none;}}
+        /* sc-component-id: sc-hcmgZB */
+        .brjtYI{max-width:1120px;width:100%;margin:auto;background-color:transparent;} @media only screen and (max-width:800px){.brjtYI{background-color:transparent;}}
+        /* sc-component-id: sc-dHmInP */
+        .Rgtyf{position:static;display:-webkit-box;display:-webkit-flex;display:-ms-flexbox;display:flex;padding:120px 0px 120px 0px;background-size:cover;background-color:rgba(0,0,0,1);background-repeat:no-repeat;background-position:50% 50%;background-attachment:initial;background-image:none;} @media only screen and (max-width:800px){.Rgtyf{width:100%;padding:80px 20px 80px 20px;background-size:cover;background-color:rgba(0,0,0,1);background-repeat:no-repeat;background-position:0% 50%;background-attachment:initial;background-image:none;}}
+        /* sc-component-id: sc-dwztqd */
+        .cbAvIN{position:relative;id:section-62028971;margin-top:2px;margin-left:0px;margin-bottom:0px;margin-right:0px;} @media only screen and (max-width:800px){.cbAvIN{margin-top:0px;margin-left:0px;margin-bottom:0px;margin-right:0px;}}
+        /* sc-component-id: sc-bOCYYb */
+        .imLhLp{position:relative;text-align:initial;margin:0px 0px 0px 0px;} @media only screen and (max-width:800px){.imLhLp{margin-top:0px;margin-right:0px;margin-bottom:0px;margin-left:0px;}}.uqayJ{position:relative;text-align:initial;display:-webkit-box;display:-webkit-flex;display:-ms-flexbox;display:flex;-webkit-flex-direction:column;-ms-flex-direction:column;flex-direction:column;margin:0px 0px 0px 0px;} @media only screen and (max-width:800px){.uqayJ{margin-top:0px;margin-right:0px;margin-bottom:0px;margin-left:0px;}}
+        /* sc-component-id: sc-dXLFzO */
+        .dICPES{display:-webkit-box;display:-webkit-flex;display:-ms-flexbox;display:flex;width:100%;height:100%;min-height:100vh;position:relative;font-family:"Open Sans";font-weight:400;font-style:normal;}
+    </style>
+    <link data-react-helmet="true" rel="canonical" href="https://quiz-master.systeme.io/master-quiz">
+    <style type="text/css">
+        @font-face {
+          font-family: "Open Sans";
+          font-style: normal;
+          font-weight: 400;
+          src: url(fonts/regular.woff2) format('woff2');
+        }
+
+
+        @font-face {
+          font-family: "Merriweather";
+          font-style: normal;
+          font-weight: 400;
+          src: url(fonts/regular_1.woff2) format('woff2');
+        }
+    </style>
+    <link rel="stylesheet" href="css/all.min.css">
+    <style>
+        #button-62a5eaf7,#button-2ac2d485,#button-f129d769,#button-cb28855e,#button-f6f8c505,#button-bed177ea,#button-0e9fc675,#button-ad7dcc75,#button-fe24d0c2,#button-fb542c98,#button-32234378 {
+          transition: transform 0.3s, color 0.3s, border-bottom-color 0.3s; /* Adding smooth transition */
+          border-bottom: 2px solid #000; /* Replace with your actual bottom border properties */
+        }
+
+        #button-62a5eaf7:hover,#button-2ac2d485:hover,#button-f129d769:hover,#button-cb28855e:hover,#button-f6f8c505:hover,#button-bed177ea:hover,#button-0e9fc675:hover,#button-ad7dcc75:hover,#button-fe24d0c2:hover,#button-fb542c98:hover,#button-32234378:hover {
+          transform: scale(1.1); /* Zoom effect on hover */
+          color: #B01E32; /* Change text color to #B01E32 on hover */
+          border-bottom-color: #B01E32; /* Change bottom border color to #B01E32 on hover */
+        }
+    </style>
+</head>
+
+<body>
+    <div id="app">
+        <div class="sc-dXLFzO dICPES">
+            <div class="sc-btzYZH lbczSC">
+                <div class="sc-jwKygS gDcHhm"></div>
+                <div class="sc-dwztqd cbAvIN">
+                    <section id="section-62028971" class="sc-dHmInP Rgtyf">
+                        <div width="wide" class="sc-hcmgZB brjtYI">
+                            <div class="sc-bOCYYb imLhLp">
+                                <div style="background-size:initial;background-position:50% 50%;background-repeat:repeat;background-attachment:initial" id="row-c0291682" class="sc-gbOuXE bmWsin">
+                                    <div size="12" class="sc-laTMn kQOYHh">
+                                        <div class="sc-bOCYYb uqayJ">
+                                            <div class="sc-kQsIoO hahKoj">
+                                              <a href="https://www.cohowe61.online/"> <picture class="sc-gPzReC kDplOt"><img id="image-3e4ab0f0" src="images/662b5abaa66cd_1.png" width="879" loading="lazy" class="sc-cpmKsF gemMjA"></picture></a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script data-cookieconsent="ignore" src="js/runtimeSimplePage.1f5d57551aa31777b361.js" async=""></script>
+    <script data-cookieconsent="ignore" src="js/simplePage.7d5a01af979c5645503b.js" async=""></script>
+    <script data-cookieconsent="ignore" src="js/vendors~simplePage.458db45ebcc6304e8c00.js" async=""></script>
+</body>
+
+</html>
